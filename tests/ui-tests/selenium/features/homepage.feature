aa12207e2a8e269fb876ce734f3fceb78b6c8eb6
John Ruggles <ruggles@catuslabs.info>
2023-11-14 15:58:56 +0000

    Ruggleson working async Selenium integration example

--- tests/ui-tests/selenium/features/homepage.feature
+++ tests/ui-tests/selenium/features/homepage.feature
@@ -5,3 +5,15 @@ Confirm that the home page loads as expected
 Scenario: Home page header loads as expected
 Given I visit the home page
 Then the home page header loads as expected
+
+Scenario: Home page logo loads as expected
+Given I visit the home page
+Then the home page logo image loads as expected
+
+Scenario: Home page sidebar loads as expected
+Given I visit the home page
+Then the home page sidebar loads as expected
+
+Scenario: Home page footer loads as expected
+Given I visit the home page
+Then the home page footer loads as expected
--- tests/ui-tests/selenium/features/step-definitions/homepage.js
+++ tests/ui-tests/selenium/features/step-definitions/homepage.js
@@ -11,4 +11,27 @@ module.exports = function () {
             await assert.equal(await header.getAttribute("innerHTML"), "ruggles online");
         });
     });
+    this.Then('the home page logo image loads as expected', async function () {
+        await this.driver.findElement(By.xpath(homepage.logo_image)).then(async function (logo) {
+            await assert.notEqual(await logo.getAttribute("naturalHeight"), 0);
+            await assert.notEqual(await logo.getAttribute("naturalWidth"), 0);
+        });
+    });
+    this.Then('the home page sidebar loads as expected', async function () {
+        await this.driver.findElement(By.xpath(homepage.sidebar_news)).then(async function (button_news) {
+            await assert.equal(await button_news.getAttribute("innerHTML"), 'news');
+        });
+        await this.driver.findElement(By.xpath(homepage.sidebar_links)).then(async function (button_links) {
+            await assert.equal(await button_links.getAttribute("innerHTML"), 'links');
+        });
+        await this.driver.findElement(By.xpath(homepage.sidebar_contact)).then(async function (button_contact) {
+            await assert.equal(await button_contact.getAttribute("innerHTML"), 'contact');
+        });
+
+    });
+    this.Then('the home page footer loads as expected', async function () {
+        await this.driver.findElement(By.xpath(homepage.footer_text)).then(async function (footer_text) {
+            await assert.equal(await footer_text.getAttribute("innerHTML"), 'copyright \xA9 2023 john ruggles, cactus labs. all rights reserved.');
+        });
+    });
 };
