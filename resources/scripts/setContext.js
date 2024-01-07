16a56813119f0a9a4816665ef2514acb453522af
John Ruggles <ruggles@catuslabs.info>
2024-01-07 23:09:20 +0000

    Ruggleson tweaked content text a little

--- resources/scripts/setContext.js
+++ resources/scripts/setContext.js
@@ -8,15 +8,16 @@ function setContext(header_name) {
 
     switch (header_name) {
         case "news":
-            content = "<p>Welcome to Ruggles online. Website under development.</p>";
+            content = "<p>welcome to ruggles online. this website is under development.</p>" +
+                "please feel free to have a sniff around.";
             break;
         case "links":
             content = "<p><a href='https://github.com/cactusLabs' id='link-github'>ruggles on github</a></p>" +
-                      "<p><a href='https://www.linkedin.com/in/john-ruggles-4437b3120/' id='link-linkedin'>ruggles on linkedIn</a></p>" +
-                      "<p><a href='https://www.youtube.com/channel/UCR4irx2OoqMu_WwKr5_BoMw' id='link-youtube'>ruggles on youtube</a></p>";
+                "<p><a href='https://www.linkedin.com/in/john-ruggles-4437b3120/' id='link-linkedin'>ruggles on linkedIn</a></p>" +
+                "<p><a href='https://www.youtube.com/channel/UCR4irx2OoqMu_WwKr5_BoMw' id='link-youtube'>ruggles on youtube</a></p>";
             break;
         case "contact":
-            content = "<p>Contact details coming soon.</p>"
+            content = "<p>contact details coming soon.</p>"
             break;
     }
     document.getElementById('content-text').innerHTML = content;
